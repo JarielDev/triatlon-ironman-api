079ada612571f9348e104f62dcf6e3fdf4b1c687
Jose Ariel <jariel.dev@hotmail.com>
2026-05-12 14:42:14 -0500

    feat: implement data cleaning logic for Strava activities using list comprehensions

--- app/strava_service.py
+++ app/strava_service.py
@@ -39,4 +39,17 @@ async def get_strava_activities():
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers=headers)
         response.raise_for_status()
-        return response.json()
+        raw_activities = response.json() # Aquí guardamos todo el JSON gigante
+
+    cleaned_data = [
+        {
+            "name": activity["name"],
+            "sport": activity["type"], 
+            "distance_km": round(activity["distance"] / 1000, 2), # De metros a km, redondeado a 2 decimales
+            "moving_time_min": round(activity["moving_time"] / 60, 2), # De segundos a minutos
+            "date": activity["start_date_local"].split("T")[0] # Cortamos la fecha
+        }
+        for activity in raw_activities
+    ]
+    
+    return cleaned_data
